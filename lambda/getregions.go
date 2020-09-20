a5b6f08b3a5305f88e8063309e5ed1c20dca698e
Gabriel Sargeant <gabe.sargeant@gmail.com>
2020-09-21 01:19:04 +1000

    lambda function tested on aws, still to write a few unit tests for it. but the basics are there for the time being

--- lambda/getregions.go
+++ lambda/getregions.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"smap/record"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -21,16 +23,11 @@ type Record struct {
 	KVPairs  map[string]float64 `json:"KVPairs"`
 }
 
-//Collection Array of Records
-type Collection struct {
-	ArrRecord []Record
-}
-
 // MapRequest  details TODO Eventually replace this with an
 // interface type for the APIGateway Request
 type MapRequest struct {
-	RegionID string `json:"RegionID"`
-	TableID  string `json:"TableID"`
+	RegionID    string `json:"RegionID"`
+	PartitionID string `json:"PartitionID"`
 }
 
 // Pointer Receiver based dependency injection
@@ -42,13 +39,19 @@ type deps struct {
 // HandleRequest Main entry point
 //This will eventually take a API Gateway Proxy Request
 //Eventually replace the response type with an API Gateway response type.
-func (d *deps) HandleRequest(ctx context.Context, request MapRequest) (string, error) {
+func (d *deps) HandleRequest(ctx context.Context, request []MapRequest) ([]record.Record, error) {
 
 	db := d.ddb
 	t := d.tableID
-	result := getData(request, db, t)
+	//result := getData(request, db, t)
 
-	return fmt.Sprintf("%s", result.Item), nil
+	result := getBatchData(request, db, t)
+
+	//Todo do something with errors in response object
+
+	//TODO method confirm requests are < 100
+
+	return result, nil
 }
 
 func main() {
@@ -61,10 +64,85 @@ func main() {
 	lambda.Start(d.HandleRequest)
 }
 
+// getBrachData - Makes requests on dynamodb with a batch interface.
+func getBatchData(requests []MapRequest, ddb dynamodbiface.DynamoDBAPI, dbTable string) []record.Record {
+
+	mapOfKeys := []map[string]*dynamodb.AttributeValue{}
+
+	for _, request := range requests {
+
+		mapOfKeys = append(mapOfKeys, map[string]*dynamodb.AttributeValue{
+			"RegionID": &dynamodb.AttributeValue{
+				S: aws.String(request.RegionID),
+			},
+			"PartitionID": &dynamodb.AttributeValue{
+				S: aws.String(request.PartitionID),
+			},
+		})
+
+	}
+
+	input := &dynamodb.BatchGetItemInput{
+		RequestItems: map[string]*dynamodb.KeysAndAttributes{
+			dbTable: &dynamodb.KeysAndAttributes{
+				Keys: mapOfKeys,
+			},
+		},
+	}
+
+	batch, err := ddb.BatchGetItem(input)
+
+	if err != nil {
+		panic(fmt.Errorf("Batch get item failed, err: %w", err))
+		processErrors(err)
+	}
+
+	var fullResults []record.Record
+
+	for _, response := range batch.Responses {
+		for _, item := range response {
+
+			var record record.Record
+			err := dynamodbattribute.UnmarshalMap(item, &record)
+			if err != nil {
+
+			}
+
+			fullResults = append(fullResults, record)
+
+		}
+	}
+	return fullResults
+}
+
+func processErrors(err error) {
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				fmt.Println(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+			case dynamodb.ErrCodeResourceNotFoundException:
+				fmt.Println(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+			case dynamodb.ErrCodeRequestLimitExceeded:
+				fmt.Println(dynamodb.ErrCodeRequestLimitExceeded, aerr.Error())
+			case dynamodb.ErrCodeInternalServerError:
+				fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+	}
+}
+
+// getData gets a single request. TODO remove this.
 func getData(request MapRequest, ddb dynamodbiface.DynamoDBAPI, dbTable string) (result *dynamodb.GetItemOutput) {
 
 	regionID := request.RegionID
-	tableID := request.TableID
+	tableID := request.PartitionID
 
 	input := &dynamodb.GetItemInput{
 		Key: map[string]*dynamodb.AttributeValue{
--- lambda/getregions_test.go
+++ lambda/getregions_test.go
@@ -12,13 +12,20 @@ import (
 
 type mockGetItems struct {
 	dynamodbiface.DynamoDBAPI
-	GetItemResponse dynamodb.GetItemOutput
-	// GetBatchItemResponse dynamodb.BatchGetItemOutput
+	GetItemResponse      dynamodb.GetItemOutput
+	GetBatchItemResponse dynamodb.BatchGetItemOutput
 }
 
 func (d mockGetItems) GetItem(in *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
 	fmt.Print("Mock!")
 	return &d.GetItemResponse, nil
+}
+
+func (d mockGetItems) BatchGetItem(in *dynamodb.BatchGetItemInput) (*dynamodb.BatchGetItemOutput, error) {
+	fmt.Print("Mock! batch Item response")
+
+	return &d.GetBatchItemResponse, nil
+
 }
 
 // TODO implement this
@@ -38,10 +45,21 @@ func TestHandleRequest(t *testing.T) {
 		tableID: "testTable",
 	}
 
-	mr := MapRequest{
-		RegionID: "123456",
-		TableID:  "G02",
+	mr0 := MapRequest{
+		RegionID:    "123456",
+		PartitionID: "G02",
 	}
+
+	mr1 := MapRequest{
+		RegionID:    "123456",
+		PartitionID: "G02",
+	}
+
+	mr := []MapRequest{}
+
+	mr = append(mr, mr0, mr1)
+
+	//fmt.Print(mr)
 
 	d.HandleRequest(context.TODO(), mr)
 
